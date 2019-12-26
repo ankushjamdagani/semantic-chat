969b8da17bc65af470c46efc8060f57f2d32069e
AnkushJ <anqush.jamdagani@gmail.com>
2019-12-26 22:03:50 +0530

    friend local search

--- client/src/components/widgets/friends-list/index.jsx
+++ client/src/components/widgets/friends-list/index.jsx
@@ -3,12 +3,42 @@ import moment from "moment";
 
 import { Status } from "__CONSTANTS";
 
-// import { TextInput, Validator } from "__COMPONENTS/shared/";
+import { TextInput } from "__COMPONENTS/shared/";
 
 import "./styles.scss";
 
 class FriendsList extends React.Component {
-  state = {};
+  state = {
+    searchString: ""
+  };
+
+  onFriendSearchChange = evt => {
+    let value = evt.target.value;
+    this.setState({
+      searchString: value
+    });
+  };
+
+  onFriendSearchSubmit = evt => {
+    if (evt.keyCode == 13) {
+      if (!this.state.searchString.trim()) {
+        return;
+      }
+    }
+  };
+
+  handleFilter = friend => {
+    const searchText = this.state.searchString;
+    if (
+      searchText &&
+      friend.email.indexOf(searchText) === -1 &&
+      friend.phone.indexOf(searchText) === -1 &&
+      friend.username.indexOf(searchText) === -1
+    ) {
+      return false;
+    }
+    return true;
+  };
 
   render = () => {
     const {
@@ -22,9 +52,18 @@ class FriendsList extends React.Component {
       <div className="widget__container friends-list__container">
         <div className="widget__container-inner">
           <div className="widget__body friends-list__body">
+            <div className="friends-search__container ts-h1">
+              <TextInput
+                name="friends-search-box"
+                value={this.state.searchString}
+                onChange={this.onFriendSearchChange}
+                onKeyDown={this.onFriendSearchSubmit}
+                placeholder="Find friends..."
+              />
+            </div>
             {status === Status.SUCCESS &&
               data &&
-              data.map((friend, index) => (
+              data.filter(this.handleFilter).map((friend, index) => (
                 <div
                   key={index}
                   onClick={() => changeActiveFriend(friend)}
@@ -43,7 +82,7 @@ class FriendsList extends React.Component {
                     <div className="friend-item__thumb ts-xl">
                       {friend.username[0]}
                     </div>
-                    <div className="friend-item__content tx-md">
+                    <div className="friend-item__content">
                       <div className="primary-content ts-md">
                         {friend.username}
                       </div>
